46844a808fb6510cdd4c166c6dd5263381284d8e
ojitos369 <ojitos369@gmail.com>
2022-08-19 16:18:21 -0500

    add react routes

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { MyComponent } from '../Components/MyComponent';
 import { useMyContext } from './useMyContext';
 
+import { Link, Route, Routes, useParams, useNavigate } from 'react-router-dom';
+
 function App() {
     const { 
         state, f,
@@ -9,12 +11,22 @@ function App() {
     } = useMyContext();
     return (
         <React.Fragment>
-            <MyComponent 
-                state={state} 
-                updateInput={f.updateInput}
-                lstate={localState}
-                lupdateInput={lf.updateInput}
+            <Routes>
+                {/* -----------   Home   ----------- */}
+                <Route
+                    path="/"
+                    element={
+                        <MyComponent 
+                            state={state} 
+                            updateInput={f.updateInput}
+                            lstate={localState}
+                            lupdateInput={lf.updateInput}
+                            />
+                    }
                 />
+                {/* -----------   Default   ----------- */}
+                <Route path="*" element={<div className='text-danger h1 text-center mt-5'>404 Not Found</div>} />
+            </Routes>
         </React.Fragment>
     );
 }
